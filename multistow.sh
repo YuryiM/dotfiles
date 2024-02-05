7d3156c3fe293b60405ed8427a4b139d9a40fce5
YM <ymironchyk@gmail.com>
2024-02-04 21:54:12 -0800

    Added stow configs

---
+++ multistow.sh
@@ -0,0 +1,30 @@
+# !/bin/zsh
+#mkdir -p gnome-session/.config/ && cp -r $DOTFILES/.config/gnome-session $DOTFILES/gnome-session/.config && stow gnome-session
+
+mkdir -p Code/.config/ && cp -r $DOTFILES/.config/Code $DOTFILES/Code/.config && stow Code
+mkdir -p dconf/.config/ && cp -r $DOTFILES/.config/dconf $DOTFILES/dconf/.config && stow dconf
+mkdir -p enchant/.config/ && cp -r $DOTFILES/.config/enchant $DOTFILES/enchant/.config && stow enchant
+mkdir -p evolution/.config/ && cp -r $DOTFILES/.config/evolution $DOTFILES/evolution/.config && stow evolution
+mkdir -p 'Github Desktop'/.config/ && cp -r $DOTFILES/.config/'Github Desktop' $DOTFILES/'Github Desktop'/.config && stow 'Github Desktop'
+mkdir -p gnome-control-center/.config/ && cp -r $DOTFILES/.config/gnome-control-center $DOTFILES/gnome-control-center/.config && stow gnome-control-center
+mkdir -p gnome-session/.config/ && cp -r $DOTFILES/.config/gnome-session $DOTFILES/gnome-session/.config && stow gnome-session
+mkdir -p goa-1.0/.config/ && cp -r $DOTFILES/.config/goa-1.0 $DOTFILES/goa-1.0/.config && stow goa-1.0
+mkdir -p gtk-3.0/.config/ && cp -r $DOTFILES/.config/gtk-3.0 $DOTFILES/gtk-3.0/.config && stow gtk-3.0
+mkdir -p gtk-4.0/.config/ && cp -r $DOTFILES/.config/gtk-4.0 $DOTFILES/gtk-4.0/.config && stow gtk-4.0
+mkdir -p i3/.config/ && cp -r $DOTFILES/.config/i3 $DOTFILES/i3/.config && stow i3
+mkdir -p ibus/.config/ && cp -r $DOTFILES/.config/ibus $DOTFILES/ibus/.config && stow ibus
+mkdir -p keepassxc/.config/ && cp -r $DOTFILES/.config/keepassxc $DOTFILES/keepassxc/.config && stow keepassxc
+mkdir -p kitty/.config/ && cp -r $DOTFILES/.config/kitty $DOTFILES/kitty/.config && stow kitty
+mkdir -p mc/.config/ && cp -r $DOTFILES/.config/mc $DOTFILES/mc/.config && stow mc
+mkdir -p nautilus/.config/ && cp -r $DOTFILES/.config/nautilus $DOTFILES/nautilus/.config && stow nautilus
+# mkdir -p nvim/.config/ && cp -r $DOTFILES/.config/nvim $DOTFILES/nvim/.config && stow nvim
+mkdir -p pulse/.config/ && cp -r $DOTFILES/.config/pulse $DOTFILES/pulse/.config && stow pulse
+mkdir -p QDirStat/.config/ && cp -r $DOTFILES/.config/QDirStat $DOTFILES/QDirStat/.config && stow QDirStat
+mkdir -p ranger/.config/ && cp -r $DOTFILES/.config/ranger $DOTFILES/ranger/.config && stow ranger
+mkdir -p rclone/.config/ && cp -r $DOTFILES/.config/rclone $DOTFILES/rclone/.config && stow rclone
+mkdir -p sway/.config/ && cp -r $DOTFILES/.config/sway $DOTFILES/sway/.config && stow sway
+mkdir -p texstudio/.config/ && cp -r $DOTFILES/.config/texstudio $DOTFILES/texstudio/.config && stow texstudio
+mkdir -p Thunar/.config/ && cp -r $DOTFILES/.config/Thunar $DOTFILES/Thunar/.config && stow Thunar
+mkdir -p waybar/.config/ && cp -r $DOTFILES/.config/waybar $DOTFILES/waybar/.config && stow waybar
+mkdir -p xfce4/.config/ && cp -r $DOTFILES/.config/xfce4 $DOTFILES/xfce4/.config && stow xfce4
+mkdir -p zsh/.config/ && cp -r $DOTFILES/.config/zsh $DOTFILES/zsh/.config && stow zsh
